b1bde98b65d97608207e2c9cd171604403b3c56d
spartan-dev <s.e.jugr@gmail.com>
2022-01-16 03:19:05 -0600

    first commit :skull:

--- src/components/layout.js
+++ src/components/layout.js
@@ -7,11 +7,17 @@
 
 import * as React from "react"
 import PropTypes from "prop-types"
-import { useStaticQuery, graphql } from "gatsby"
+import { useStaticQuery, graphql, Link } from "gatsby"
 
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faCoffee, faList } from "@fortawesome/free-solid-svg-icons"
+import {
+  faFacebookF,
+  faInstagram,
+  faWhatsapp,
+} from "@fortawesome/free-brands-svg-icons"
 import Header from "./header"
 import "./layout.css"
-
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
     query SiteTitleQuery {
@@ -25,25 +31,37 @@ const Layout = ({ children }) => {
 
   return (
     <>
-      <Header siteTitle={data.site.siteMetadata?.title || `Title`} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0 1.0875rem 1.45rem`,
-        }}
-      >
-        <main>{children}</main>
-        <footer
-          style={{
-            marginTop: `2rem`,
-          }}
-        >
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.com">Gatsby</a>
-        </footer>
+      <nav className="navbar">
+        <div className="sectionNav">
+          <ul className="list">
+            {/*  <li>
+              <FontAwesomeIcon icon={faList} size="2x" />
+            </li> */}
+          </ul>
+        </div>
+        <div className="sectionNav">
+          <div className="socialMedia">
+            <a href="https://www.facebook.com/basiliscusrex">
+              <FontAwesomeIcon icon={faFacebookF} size="2x" />
+            </a>
+            <a href="https://www.instagram.com/basiliscus_oficial/">
+              <FontAwesomeIcon icon={faInstagram} size="2x" />
+            </a>
+            <a href="https://api.whatsapp.com/send?phone=5586824354">
+              <FontAwesomeIcon icon={faWhatsapp} size="2x" />
+            </a>
+          </div>
+        </div>
+      </nav>
+      <div className="container mx-auto flex-1 justify-center items-center">
+        <h1 className="mt-4 text-xl font-bold sm:text-xl text-red-500 hover:text-red-600">
+          Aun seguimos en contruccion
+        </h1>
       </div>
+      <p className="mt-4 pl-4  text-xl font-bold sm:text-2xl text-white hover:text-red-600">
+        Pero puedes visitarnos por el momento en nuestras redes{" "}
+      </p>
+      <main>{children}</main>
     </>
   )
 }
